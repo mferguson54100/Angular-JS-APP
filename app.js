93eab1af0761b439a2e616448489c1586e379882
Michael Ferguson <mferguson54100@gmail.com>
2019-01-28 11:00:21 -0800

    adding game functions to select random words from an array

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ app.controller("GameController",['$scope',function($scope){
     
     $scope.incorrectLettersChosen=[];
     $scope.correctLetters=[];
+    $scope.numberOfGuesses =6;
+    $scope.displayWord='';
+    $scope.input={
+        letter:''
+    }
+
+    let selectRandomWord = function(){
+        let index = Math.round(Math.random()*words.length);
+        return words[index];
+
+    }
+let newGame = function(){
+    $scope.incorrectLettersChosen =[];
+    $scope.correctLetters =[];
+    $scope.numberOfGuesses =6;
+    $scope.displayWord='';
+    $scope.input={
+        letter:''
+    }
+
+    selectedWord = selectRandomWord();
+    console.log(selectedWord);
+
+}
+newGame();
 
 
 }]);
